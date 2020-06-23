e5940c9b930d0006e5d78e8b66ebf9546e035811
hishamss <hishamss90@gmail.com>
2020-06-23 02:38:30 -0700

    test on heroku

--- models/book.js
+++ models/book.js
@@ -6,7 +6,7 @@ const bookSchema = new Schema({
   authors: [],
   description: { type: String },
   image: { type: String },
-  infoLink: { type: String },
+  infoLink: { type: String, unique: true },
 });
 
 const Book = mongoose.model("books", bookSchema);
