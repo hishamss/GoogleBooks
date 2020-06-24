f31edaf71af257a1c7b5bb3a0ff899c0b94691a0
hishamss <hishamss90@gmail.com>
2020-06-23 19:46:18 -0700

    userlocation.pathname to activate the current clicked link

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -8,13 +8,25 @@ function Nav() {
       </Link>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                useLocation().pathname === "/" ? "nav-link active" : "nav-link"
+              }
+            >
               Search
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/saved">
+            <Link
+              to="/saved"
+              className={
+                useLocation().pathname === "/saved"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
               Saved
             </Link>
           </li>
